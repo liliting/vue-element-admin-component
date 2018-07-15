188a7b9de7b347bb5c745a90c59b86d9bd004ff0
liliting <18335832714@163.com>
2018-07-15 19:12:56 +0800

    axios请求拦截与相应拦截

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { Message } from 'element-ui'
 
 const service = axios.create({
   baseURL: '',
@@ -11,5 +12,24 @@ service.interceptors.request.use(config => {
   console.log(error)
   Promise.reject(error)
 })
+
+service.interceptors.response.use(
+  response => {
+    const res = response.data
+    if (res.code !== 200) {
+      Message({
+        message: res.msg,
+        diration: 5000,
+        type: 'error'
+      })
+    } else {
+      return res.data
+    }
+  },
+  error => {
+    console.log(error)
+    return Promise.reject(error)
+  }
+)
 
 export default service
