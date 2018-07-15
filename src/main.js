dd9763b6f6332949fc6070bf628d5880cff9e481
liliting <18335832714@163.com>
2018-07-15 19:15:04 +0800

    引入normalize.css

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
 import App from './App'
 import router from './router'
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import 'normalize.css'
 
 Vue.use(ElementUI)
 
