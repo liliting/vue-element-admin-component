f7fe565a82415457b9b8d17207c890c173edba6c
liliting <18335832714@163.com>
2018-08-22 18:26:34 +0800

    upload组件

---
+++ src/components/form/SkUpload.vue
@@ -0,0 +1,34 @@
+<template>
+  <el-form-item :label="field.name" v-if="!field.primaryKey">
+    <el-upload class="avator-upload" action="">
+      <img v-if="imgUrl" :src="imgUrl" alt="img" class="avatar">
+      <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+    </el-upload>
+  </el-form-item>
+</template>
+
+<script>
+export default {
+  name: 'SkInput',
+  props: [ 'field' ],
+  data () {
+    return {
+      imgUrl: ''
+    }
+  },
+  methods: {
+    checkElementLegal () {
+      return this.field.value.length > 0
+    },
+    getElementData () {
+      var result = {}
+      result[this.field.field] = this.field.value
+      return result
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
