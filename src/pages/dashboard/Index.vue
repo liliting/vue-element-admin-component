7b82706afb771cda7ed982ebe7e01cad604acaae
liliting <18335832714@163.com>
2018-09-21 15:50:46 +0800

    scss模块化

--- src/pages/dashboard/Index.vue
+++ src/pages/dashboard/Index.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="login-container">
-    dashboard
+    <el-row :gutter="20">
+      <el-col :span="6">
+        <el-card>
+          <div slot="header" class="blue">昨日访问量</div>
+          <div>10000</div>
+        </el-card>
+      </el-col>
+      <el-col :span="6">
+        <el-card>
+          <div slot="header" class="success">用户量</div>
+          <div>5000</div>
+        </el-card>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -22,5 +35,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-
+.login-container{
+  padding:20px;
+}
 </style>
